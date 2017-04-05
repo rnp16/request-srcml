9c659d9c5574273204003632c2db0cfec9203de5
rnp16 <rnp16@zips.uakron.edu>
2017-04-04 21:39:19 -0400

    Added Test Case 6

--- request_srcml_t.cpp
+++ request_srcml_t.cpp
@@ -9,20 +9,20 @@
 
 int main() {
 
-	// all filenames and languages empty
-	//Initial Test
+     // all filenames and languages empty
+    //Initial Test
     {
     	srcml_request request = { "", "", "", ""};
     	assert(request_filename(request) == "");
     	assert(request_language(request, "") == "");
     }
-	/*The local_filename is the name of the file on the disk. For a source-code archive the entry_filename is used instead*/
+    /*The local_filename is the name of the file on the disk. For a source-code archive the entry_filename is used instead*/
 	{
      	srcml_request request = { "", "code.tar.gz", "test.cpp", ""};
      	assert(request_filename(request) == "test.cpp");
      	assert(request_language(request, "") == "");
-     }
-        /*For a plain old file the entry_filename is the literal string "data" and you should use the local_filename*/
+    }
+    /*For a plain old file the entry_filename is the literal string "data" and you should use the local_filename*/
     {
         srcml_request request = { "", "test.cpp", "data", ""};
         assert(request_filename(request) == "test.cpp");
@@ -46,6 +46,15 @@ int main() {
         assert(request_filename(request) == "test.cpp");
         assert(request_language(request, "test.cpp") == "C++");
     }
+    /*The local_filename is a single dash when the input is from stdin. in this case the option_filename and entry_filename must be used.
+     Note that the entry_filename is not valid for a non source code archive and the option_filename must be used.
+     If the option_filename is blank then the resulting filename is blank and the language is based on the option_language.*/
+    {
+        srcml_request request = {"", "-" "data", ""};
+        assert(request_filename(request) == "");
+        assert(request_language(request, "") == "");
+    }
+
 
     return 0;
 }

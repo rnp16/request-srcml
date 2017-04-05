4a29f0b1b2ad1ae72cb8a9530f2556a35b6d85a0
rnp16 <rnp16@zips.uakron.edu>
2017-04-04 21:30:05 -0400

    Added Test Case 2

--- request_srcml_t.cpp
+++ request_srcml_t.cpp
@@ -22,6 +22,12 @@ int main() {
      	assert(request_filename(request) == "test.cpp");
      	assert(request_language(request, "") == "");
      }
+	/*For a plain old file the entry_filename is the literal string "data" and you should use the local_filename*/
+    {
+        srcml_request request = { "", "test.cpp", "data", ""};
+        assert(request_filename(request) == "test.cpp");
+        assert(request_language(request, "") == "");
+     }
 
     return 0;
 }

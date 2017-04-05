a674fab5661770a86805c1c16a4cc773a9bed2fb
rnp16 <rnp16@zips.uakron.edu>
2017-04-04 21:27:44 -0400

    Added Test Case 1

--- request_srcml_t.cpp
+++ request_srcml_t.cpp
@@ -16,6 +16,12 @@ int main() {
     	assert(request_filename(request) == "");
     	assert(request_language(request, "") == "");
     }
+	/*The local_filename is the name of the file on the disk. For a source-code archive the entry_filename is used instead*/
+	{
+     	srcml_request request = { "", "code.tar.gz", "test.cpp", ""};
+     	assert(request_filename(request) == "test.cpp");
+     	assert(request_language(request, "") == "");
+     }
 
     return 0;
 }
